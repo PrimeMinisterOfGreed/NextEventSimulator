eb860bf998c92aef358649a1c60d7bb0019673f4
drfaust <matte.ielacqua@gmail.com>
2023-01-20 19:05:41 +0100

    add options for main project

---
+++ Scheduler/include/Options.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#include <boost/program_options/variables_map.hpp>
+
+extern boost::program_options::variables_map & optionsMap;
--- Scheduler/src/main.cpp
+++ Scheduler/src/main.cpp
@@ -1,6 +1,22 @@
+#include <boost/program_options.hpp>
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <boost/type_traits/promote.hpp>
+#include <iostream>
 
-
-int main()
+boost::program_options::variables_map & optionsMap = *new boost::program_options::variables_map();
+int main(int argc, char ** argv)
 {
-    
+    using namespace boost::program_options;
+    options_description descr("Allowed options");
+    descr.add_options()("help", "display this help");
+    store(parse_command_line(argc, argv, descr), optionsMap);
+    notify(optionsMap);
+
+    if (optionsMap.count("help"))
+    {
+        std::cout << descr << std::endl;
+    }
+
 }
