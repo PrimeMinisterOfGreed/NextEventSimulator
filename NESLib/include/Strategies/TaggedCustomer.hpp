8f50cbe4c2b7599bd6b461a30a2fdebba83e6ba7
DrFaust <matteo.ielacqua@gmail.com>
2024-01-11 19:07:47 +0100

    add TaggedCustomer approach

--- NESLib/include/Strategies/TaggedCustomer.hpp
+++ NESLib/include/Strategies/TaggedCustomer.hpp
@@ -1,4 +1,15 @@
 #pragma once
+#include "Event.hpp"
+#include "Measure.hpp"
+#include "Shell/SimulationShell.hpp"
+#include "Station.hpp"
+#include <map>
+#include <utility>
 struct TaggedCustomer
 {
+    Accumulator<> _meanTimes;
+    std::map<std::string, std::pair<double, double>> _times;
+    void ConnectEntrance(BaseStation * station);
+    void ConnectLeave(BaseStation * station);
+    void AddShellCommands(SimulationShell* shell);
 };
--- NESLib/src/TaggedCustomer.cpp
+++ NESLib/src/TaggedCustomer.cpp
@@ -1,1 +1,25 @@
+#include "Core.hpp"
+#include "Event.hpp"
 #include <Strategies/TaggedCustomer.hpp>
+#include <fmt/core.h>
+
+void TaggedCustomer::ConnectEntrance(BaseStation *station)
+{
+    station->OnArrival([this](auto s, Event &e) { _times.at(e.Name) = {e.OccurTime, 0}; });
+}
+
+void TaggedCustomer::ConnectLeave(BaseStation *station)
+{
+    station->OnDeparture([this](auto s, Event &e) {
+        if (_times.count(e.Name) == 0)
+            panic(fmt::format("Event with name {} not registered at entrance", e.Name));
+        _times.at(e.Name).second = e.OccurTime;
+        auto pair = _times.at(e.Name);
+        _meanTimes(pair.second - pair.first);
+    });
+}
+
+void TaggedCustomer::AddShellCommands(SimulationShell *shell)
+{
+    
+}
